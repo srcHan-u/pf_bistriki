d4c0376e3b2866e57c65466a678b5b335a3e19ca
Han <boikoale97@gmail.com>
2022-09-13 20:43:56 +0300

    made reviews section

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -7,7 +7,7 @@
 // Подключаем слайдер Swiper из node_modules
 // При необходимости подключаем дополнительные модули слайдера, указывая их в {} через запятую
 // Пример: { Navigation, Autoplay }
-import Swiper, { Navigation, EffectFade } from 'swiper';
+import Swiper, { Navigation, EffectCoverflow, Pagination } from 'swiper';
 /*
 Основниые модули слайдера:
 Navigation, Pagination, Autoplay, 
@@ -24,6 +24,7 @@ import "../../scss/base/swiper.scss";
 // import 'swiper/css';
 
 // Инициализация слайдеров
+
 function initSliders() {
 	// Перечень слайдеров
 	// Проверяем, есть ли слайдер на стронице
@@ -37,6 +38,7 @@ function initSliders() {
 			observeParents: true,
 			slidesPerView: 4,
 			spaceBetween: 24,
+            grabCursor: true,
 			autoHeight: false,
 			speed: 800,
 
@@ -45,7 +47,7 @@ function initSliders() {
 			//loop: true,
 			//preloadImages: false,
 			lazy: true,
-
+     
 			/*
 			// Эффекты
 			autoplay: {
@@ -92,6 +94,85 @@ function initSliders() {
 					spaceBetween: 30,
 				},
 			},
+			// События
+			on: {
+
+			}
+		});
+	}
+	if (document.querySelector('.reviews-slider')) { // Указываем скласс нужного слайдера
+		// Создаем слайдер
+		new Swiper('.reviews-slider', { // Указываем скласс нужного слайдера
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Navigation, EffectCoverflow, Pagination],
+			observer: true,
+			observeParents: true,
+			slidesPerView: 1,
+			spaceBetween: 5,
+			autoHeight: false,
+			speed: 800,
+            grabCursor: false,
+            allowTouchMove: false,
+			// touchRatio: 0.5,
+			// simulateTouch: true,
+			//loop: true,
+			//preloadImages: false,
+			lazy: false,
+            // effect: 'coverflow',
+            coverflowEffect: {
+            //   rotate: 70,
+            //   slideShadows: false,
+            //   depth: 10,
+            },
+			/*
+			// Эффекты
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+			// Пагинация
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'fraction',
+                el: '.reviews-slider-controls__pagination',
+				clickable: true,
+              },
+			/*
+			*/
+
+			// Скроллбар
+			/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+			// Кнопки "влево/вправо"
+			navigation: {
+				prevEl: '.reviews-slider-controls__button-prev',
+				nextEl: '.reviews-slider-controls__button-next',
+			},
+
+			// Брейкпоинты
+			// breakpoints: {
+			// 	320: {
+            //         slidesPerView: 1.25,
+            //         spaceBetween: 17,
+            //         // autoHeight: true,
+			// 	},
+			// 	768: {
+			// 		slidesPerView: 2.5,
+			// 		spaceBetween: 20,
+			// 	},
+			// 	1268: {
+			// 		slidesPerView: 4,
+			// 		spaceBetween: 30,
+			// 	},
+			// },
 			// События
 			on: {
 
